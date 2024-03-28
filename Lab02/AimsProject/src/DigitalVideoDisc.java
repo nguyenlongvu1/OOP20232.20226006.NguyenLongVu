f10ee4c719a658ba46106c2a792ec03e2751fb24
nguyenlongvu1 <longvucfc2307@gmail.com>
2024-03-29 00:13:20 +0700

    Lab02

--- Lab02/AimsProject/src/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/DigitalVideoDisc.java
@@ -5,19 +5,24 @@ public class DigitalVideoDisc
     private String director;
     private int lenght;
     private float cost;
+    private String id;
+    private static int nbDigitalVideoDisc=0;
     public DigitalVideoDisc(String title) {
         this.title = title;
+        nbDigitalVideoDisc+=1;
     }
     public DigitalVideoDisc(String title, String category, float cost) {
         this.title = title;
         this.category = category;
         this.cost = cost;
+        nbDigitalVideoDisc+=1;
     }
     public DigitalVideoDisc(String title, String category, String director, float cost) {
         this.title = title;
         this.category = category;
         this.director = director;
         this.cost = cost;
+        nbDigitalVideoDisc+=1;
     }
     public DigitalVideoDisc(String title, String category, String director, int lenght, float cost) {
         this.title = title;
@@ -25,6 +30,16 @@ public class DigitalVideoDisc
         this.director = director;
         this.lenght = lenght;
         this.cost = cost;
+        nbDigitalVideoDisc+=1;
+    }
+    
+    public DigitalVideoDisc(String title, String category, String director, int lenght, float cost, String id) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.lenght = lenght;
+        this.cost = cost;
+        this.id = id;
     }
     public String getTitle() {
         return title;
@@ -40,6 +55,15 @@ public class DigitalVideoDisc
     }
     public float getCost() {
         return cost;
+    }
+    public String getId() {
+        return id;
+    }
+    public static int getNbDigitalVideoDisc() {
+        return nbDigitalVideoDisc;
+    }
+    public void setTitle(String title) {
+        this.title = title;
     }
     
 }
